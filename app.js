9192f39dc64b1249b3dfebdacb79469f537b8073
Ogweno-D <dennisogwene@gmail.com>
2024-07-05 14:13:26 +0300

    .

--- app.js
+++ app.js
@@ -5,8 +5,15 @@ dotenv.config()
 const app = express();
 
 const PORT = process.env.PORT || 5001;
-app.get('/api/v1/')
+
+app.get('/api/v1/hello', (req, res) => {
+    res.json(
+        {
+            message : "Hello world"
+        }
+    );
+  });
 
 app.listen(PORT,()=>{
-
+    console.log('Server is running at port ',PORT)
 })
